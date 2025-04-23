ca0f0d34efbd1cc23ece76df835453b0a8400e47
Abhishek Pathak <abhishekpathak37733@gmail.com>
2025-04-24 01:23:53 +0530

    commit

--- Backend/Server/Socket/socket.js
+++ Backend/Server/Socket/socket.js
@@ -1,39 +1,3 @@
-// import { Server } from "socket.io";
-// import http from "http";
-// import express from "express";
-
-// const app = express();
-
-// const server = http.createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     origin: ["http://localhost:5173"],
-//     methods: ["GET", "POST"],
-//     credentials: true,
-//   },
-// });
-
-// export const getReciverSocketId = (receiverId) => {
-//   return userSocketmap[receiverId];
-// };
-
-// const userSocketmap = {}; //{userId,socketId}
-// io.on("connection", (socket) => {
-//   const userId = socket.handshake.query.userId;
-
-//   if (userId) {
-//     userSocketmap[userId] = socket.id;
-//   }
-//   io.emit("getOnlineUsers", Object.keys(userSocketmap));
-
-//   socket.on("disconnect", () => {
-//     delete userSocketmap[userId],
-//       io.emit("getOnlineUsers", Object.keys(userSocketmap));
-//   });
-// });
-
-// export { app, io, server };
-
 // socket.js
 import { Server } from "socket.io";
 import http from "http";
